89ec22fdbbd9e74575d6c853cec53f4546009793
mohit-rajput-raj <rajputmohit19101169@gmail.com>
2026-09-01 22:16:46 +0530

    letcode-4034-1

--- leetCode/java/medium/4034.java
+++ leetCode/java/medium/4034.java
@@ -1,9 +1,7 @@
 class Solution {
     public int minBishopMoves(int[] s, int[] t) {
         if((s[0]+s[1])%2 != (t[0]+ t[1])%2)return -1;
-        int a = Math.abs(s[0]-t[0]);
-        int b = Math.abs(s[1]-t[1]);
-        if(a!=b)return 2;
+        if(Math.abs(s[0]-t[0])!=Math.abs(s[1]-t[1]))return 2;
         return 1;
     }
 }
